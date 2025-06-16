faeff0e242cfdcfbd4086dc9f1175a0e31f8f1b5
mercedesbr <mercrodriguez@uade.edu.ar>
2025-06-16 17:20:34 -0300

    sumando js al form de cntacto

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function mostrarMensaje(event){
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let asunto = document.getElementById("asunto").value;
+    let mensaje = document.getElementById("mensaje").value;
+
+    let contenedorMensaje = document.getElementById("mensajeConfirmacion");
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (!nombre || !email || !asunto || !mensaje) {
+        contenedorMensaje.textContent = "Por favor, completa todos los campos.";
+        return;
+    }
+    
+    if (!regexEmail.test(email)) {
+        contenedorMensaje.textContent = "Por favor, ingresa un correo electrónico válido.";
+        return;
+    }
+
+    let mensajeEnvio = `Hola, ${nombre}! Gracias por tu mensaje, en breve nos estaremos comunicando contigo.`;
+    contenedorMensaje.textContent = mensajeEnvio;
+  }
